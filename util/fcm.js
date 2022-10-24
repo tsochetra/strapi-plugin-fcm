89b9f5fcbcf8dc7132183d32ad5435229a5ffe17
tsochetra <than.sochetra@icloud.com>
2022-10-24 12:02:55 +0700

    update android priority

--- util/fcm.js
+++ util/fcm.js
@@ -22,6 +22,7 @@ module.exports = {
             },
             topic: topic,
             android:{
+              priority: 'high',
               notification: {
                color: "#063159"
               }
